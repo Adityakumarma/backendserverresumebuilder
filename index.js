4b7b72485669f39bde410f1f9c873ea8394c165d
Adityakumarma <adityakumarma@gmail.com>
2025-10-15 12:26:55 +0530

    indexfile added

---
+++ index.js
@@ -0,0 +1,26 @@
+// import json server
+const JSONServer = require("json-server")
+
+// create server for running json file
+const rBuilder = JSONServer.create()
+
+// create middleware
+const middleware = JSONServer.defaults()
+
+// import db.json file
+const router = JSONServer.router("db.json")
+
+// define port to run server
+const PORT = 3000
+
+// use middleware
+rBuilder.use(middleware)
+
+// use router
+rBuilder.use(router)
+
+// Listen for client requests
+rBuilder.listen(PORT,()=>{
+    console.log(`Resume Builder Started at Port number ${PORT}`);
+    
+})
